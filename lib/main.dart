0540c9a3cb74fd77596fa6d607e2a7fa1bf7b9ad
vugarismayil24 <mrsoldier799@gmail.com>
2024-05-28 18:21:10 +0400

    debug mode false

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: WeatherScreen(),
     );
   }
